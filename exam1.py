c9fac0e0a54a2232a3435c1c7c30508731fcd8b4
smart-al <99301960+smart-al@users.noreply.github.com>
2022-06-06 11:40:18 -0700

    Fixes Image that is wrong size and rotates it if not right side up.

---
+++ exam1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os, sys
+from PIL import Image
+
+size = (128,128)
+
+for infile in os.listdir():
+    outfile = os.path.splitext(infile) [0]
+
+    try:
+        with Image.open(infile).convert('RGB') as im:
+            im.thumbnail(size)
+            im.rotate(270).save("/opt/icons/" + outfile, "JPEG")
+    except OSError:
+        pass
